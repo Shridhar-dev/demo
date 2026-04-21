6534276ead40e27e9f4cf81bf24c27ba57515614
Shridhar Kamat <52820662+Shridhar-dev@users.noreply.github.com>
2026-04-22 03:06:51 +0530

    Refactor console.log statements in script.js
    Removed duplicate console.log statements and cleaned up formatting.

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
 
 console.log(1+2+3+4+5)
-console.log(1+2+3+4+5)console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
-console.log(1+2+3+4+5)console.log(1+2+3+4+5)
+console.log(1+2+3+4+5)
+console.log(1+2+3+4+5)
+console.log(1+2+3+4+5)
+  
+console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
