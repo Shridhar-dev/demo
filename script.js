0fc23a778e555b18facfc15dd1779d2feab47614
Shridhar Kamat <shridharkamat10@gmail.com>
2026-04-22 03:11:23 +0530

    feat: add cowsay

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import cowsay from "cowsay"
+
 console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
@@ -12,3 +14,11 @@ console.log(1+2+3+4+5)
   
 console.log(1+2+3+4+5)
 console.log(1+2+3+4+5)
+
+
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
